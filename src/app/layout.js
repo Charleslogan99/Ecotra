4b38e1915ed7707ba4717a3e435c824b3f95d5ab
Charly <charlesokoro15@gmail.com>
2025-01-21 04:45:21 +0100

    added hero section to the homepage

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,22 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const roboto = Roboto({
+  subsets: ["latin"], // Subsets for the font
+  weight: ["400", "700"], // Specify the font weights you need
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -19,11 +26,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <body className={`${roboto.variable} antialiased`}>
+      {children}
+    </body>
+  </html>
   );
 }
